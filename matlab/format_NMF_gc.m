120d0dc0f0d28fd9bafbb0f03bba9d344aab1029
jastiso <jeni.stiso@gmail.com>
2018-07-18 11:23:20 -0400

    added independent phase randomization null model.

--- matlab/format_NMF_gc.m
+++ matlab/format_NMF_gc.m
@@ -69,7 +69,7 @@ end
 
 save([top_dir, 'Behavior/behavior_all.mat'], 'behavior_all');
 
-%% Repeat for time shift
+%% Repeat for uniform phase randomization
 
 cnte = 0;
 errors_pr = [];
@@ -121,3 +121,52 @@ end
 
 
 
+%% Repeat for independent phase randomiztion
+
+cnte = 0;
+errors_pr = [];
+nTrial = 0;
+
+for e = 1:numel(elecs)
+    eType = elecs{e};
+    for f  = 1:numel(bands)
+        freq = bands{f};
+        for i = subjs
+            try
+                A = [];
+                subj = sprintf('%03d', i);
+                for j = 1:numel(sessions)
+                    sess = sessions{j};
+                    for k = 1:numel(condition)
+                        cond = condition{k};
+                        load([top_dir, sess, '/', cond, '/', subj, '/FCmatrices/NMF_', freq, '_', eType, '_ind.mat']);
+                        
+                        eval(['A = [A, gc_', num2str(eType), '];']);
+                        eval(['nTrial(j,k) = size(gc_', num2str(eType), ',2);'])
+                        
+                    end
+                    
+                end
+                if any(any(isnan(A)))
+                    display([num2str(i), ' ', num2str(f)])
+                    warning('This matrix has NaNs')
+                end
+                if any(any(~isreal(A)))
+                    display([num2str(i), ' ', num2str(f)])
+                    warning('This matrix has complex numbers')
+                end
+                %scale perf
+                eval(['sf = mean(mean(gc_', num2str(eType), '(gc_', num2str(eType), '~=0)));']);
+                
+                behavior = interp_perf(behavior_updated, i, 6, nTrial);
+                behavior = behavior.*sf;
+                A = [A; behavior];
+                save([save_dir, 'ind_gc_', freq, '_', eType, '_', subj], 'A')
+                
+            catch
+                cnte = cnte + 1;
+                errors_pr{cnte} = [top_dir, sess, '/', cond, '/', subj, '/FCmatrices/NMF_', freq, '_', eType, 'ind_gc.mat'];
+            end
+        end
+    end
+end
