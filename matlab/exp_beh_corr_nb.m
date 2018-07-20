87734c8e3c242c19f04f788428fb2e7b15a07c01
jastiso <jeni.stiso@gmail.com>
2018-07-20 15:41:50 -0400

    added code for unimodal (no behavior) data

---
+++ matlab/exp_beh_corr_nb.m
@@ -0,0 +1,215 @@
+%% Get correlation between expression and behavior
+% checking relationships to learning for nb NMF
+
+addpath(genpath('/Users/stiso/Documents/MATLAB/npy-matlab-master/'))
+addpath(genpath('/Users/stiso/Documents/MATLAB/mult_comp_perm_corr/'))
+addpath('/Users/stiso/Documents/MATLAB/fieldtrip-20170830/')
+R_dir_o = '/Users/stiso/Documents/R/NetBCI/data/gc/';
+
+top_dir = '/Users/stiso/Documents/MATLAB/NetBCI/';
+data_dir = '/Users/stiso/Documents/Python/NetBCI/NMF/gc/';
+save_dir = [top_dir, 'GroupAvg/analysis/'];
+img_dir = [top_dir, 'GroupAvg/images/'];
+% make directories
+if ~exist(img_dir, 'dir')
+    mkdir(img_dir)
+end
+% make directories
+if ~exist(save_dir, 'dir')
+    mkdir(save_dir)
+end
+
+Subj = [1:20];
+nSubj = numel(Subj);
+freqs = [7,14;15,30;31,45;55,70];
+bands = [{'alpha'},{'beta'},{'low_gamma'},{'gamma'}];
+
+sensors = [{'grad'}];
+% load behavior
+load([top_dir, 'Behavior/behavior_all'])
+load([top_dir, 'Behavior/stats'])
+%initialize
+max_corrs = zeros(nSubj, numel(bands), numel(sensors));
+min_corrs = zeros(nSubj, numel(bands), numel(sensors));
+max_exp = zeros(nSubj, numel(bands), numel(sensors));
+min_exp = zeros(nSubj, numel(bands), numel(sensors));
+sum_exp = zeros(nSubj, numel(bands), numel(sensors));
+sd_exp = zeros(nSubj, numel(bands), numel(sensors));
+
+
+%% Loop through data
+
+for j = 1:numel(sensors)
+    sens = sensors{j};
+    R_dir = [R_dir_o, sens, '/'];
+    
+    for i = Subj
+        idx = find(i == Subj);
+        subj = sprintf('%03d', i);
+        
+        for k = 1:numel(bands)
+            f = bands{k};
+            
+            % get subgraph data
+            subset = readNPY([data_dir, subj,  '/', sens, '/nb_gc_', f, '_subset.npy']);
+            coeff = readNPY([data_dir, subj,  '/', sens, '/nb_gc_', f, '_coeff.npy']);
+
+            
+            % get corr
+            max_corrs(idx,k,j) = max(corr(behavior_all{i}', coeff'));
+            min_corrs(idx,k,j) = min(corr(behavior_all{i}', coeff'));
+            figure(1); clf
+
+        end
+    end
+    
+    
+    % make picture
+    figure(1)
+    imagesc(max_corrs);
+    caxis([min(min(min_corrs)), max(max(max_corrs))])
+    colorbar
+    xlabel('Frequency')
+    ylabel('Subject')
+    saveas(gca, [save_dir, 'behavior_exp_corr.png'], 'png')
+    save([R_dir, 'max_exp_b_corr_nb.mat'], 'max_corrs')
+    save([R_dir, 'min_exp_b_corr_nb.mat'], 'min_corrs')
+    
+    figure(2)
+    imagesc(min_corrs);
+    caxis([min(min(min_corrs)), max(max(max_corrs))])
+    colorbar
+    xlabel('Frequency')
+    ylabel('Subject')
+    saveas(gca, [save_dir, 'min_behavior_exp_corr_nb.png'], 'png')
+    
+    % plot realtionship between slope of learning and expression
+    for i = 1:numel(bands)
+        corrplot([betas(Subj)', improvement(Subj)', max_corrs(:,i), min_corrs(:,i)], 'Var', {'Beta', 'Im', 'Max', 'min'}, 'type', 'pearson','testr', 'on')
+        pause(0.001)
+    end
+    
+    figure(1); clf
+    corrplot([repmat(betas(Subj)',numel(bands),1), repmat(improvement(Subj)',numel(bands),1),  ...
+        reshape(max_corrs,[],1), reshape(min_corrs,[],1)], 'Var',...
+        {'Beta',  'Im', 'Max', 'Min'}, 'type', 'spearman','testr', 'on')
+    
+    % save to R_dir
+    save([R_dir, 'exp_beh_cor_nb.mat'], 'betas',  'max_exp', 'sum_exp', 'sd_exp');
+    
+end
+
+
+
+% select individual corrplot
+% figure(1); clf
+% s1 = plot(betas(Subj)', max_corrs(:,1), 'k.', 'color', [.7,.7,.7]);
+% set(s1, 'MarkerSize', 20, 'LineWidth', 2);
+% %%% regression line
+% hold on
+% l = lsline ;
+% set(l,'color', 'k', 'LineWidth', 2)
+% hold off
+% [p,r] = mult_comp_perm_corr(betas(Subj)', max_exp(:,1))
+% saveas(gca, [save_dir, 'max_beta_alpha_nb.png'], 'png')
+% 
+% figure(1); clf
+% s1 = plot(betas(Subj)', sd_exp(:,1), 'k.', 'color', [.7,.7,.7]);
+% set(s1, 'MarkerSize', 20, 'LineWidth', 2);
+% %%% regression line
+% hold on
+% l = lsline ;
+% set(l,'color', 'k', 'LineWidth', 2)
+% hold off
+% [p,r] = mult_comp_perm_corr(betas(Subj)', sd_exp(:,1))
+% saveas(gca, [save_dir, 'sd_beta_alpha_nb.png'], 'png')
+% 
+% figure(1); clf
+% s1 = plot(betas(Subj)', max_exp(:,2), 'k.', 'color', [.7,.7,.7]);
+% set(s1, 'MarkerSize', 20, 'LineWidth', 2);
+% %%% regression line
+% hold on
+% l = lsline ;
+% set(l,'color', 'k', 'LineWidth', 2)
+% hold off
+% [p,r] = mult_comp_perm_corr(betas(Subj)', max_exp(:,2))
+% saveas(gca, [save_dir, 'max_beta_beta_nb.png'], 'png')
+% 
+% figure(1); clf
+% s1 = plot(betas(Subj)', sd_exp(:,2), 'k.', 'color', [.7,.7,.7]);
+% set(s1, 'MarkerSize', 20, 'LineWidth', 2);
+% %%% regression line
+% hold on
+% l = lsline ;
+% set(l,'color', 'k', 'LineWidth', 2)
+% hold off
+% [p,r] = mult_comp_perm_corr(betas(Subj)', sd_exp(:,2))
+% saveas(gca, [save_dir, 'sd_beta_beta_nb.png'], 'png')
+
+
+
+
+
+% %% Now for BL
+%
+% % Loop through data
+%
+% for i = 2:5
+%     subj = sprintf('%03d', i);
+%     for j = 1:numel(sensors)
+%         sens = sensors{j};
+%         for k = 1:numel(bands)
+%             f = bands{k};
+%
+%             % get subgraph data
+%             subset = readNPY([data_dir, subj, '/', f, '_subset_bl.npy']);
+%             coeff = readNPY([data_dir, subj, '/', f, '_coeff_bl.npy']);
+%
+%             %get subgraph with highest expression
+%             [max_e,bsg] = max(coeff(:,end));
+%             [min_e,nbsg] = min(coeff(:,end));
+%
+%             % get corr
+%             max_corrs(i,k,j) = corr(perf(:,i), subset(bsg,:)');
+%             min_corrs(i,k,j) = corr(perf(:,i), subset(nbsg,:)');
+%             max_exp(i,k,j) = max_e;
+%             min_exp(i,k,j) = min_e;
+%             sum_exp(i,k,j) = mean(coeff(:,end));
+%             sd_exp(i,k,j) = std(coeff(:,end));
+%             hist(coeff(:,end))
+%             pause(0.001)
+%         end
+%     end
+% end
+%
+% % make picture
+% figure(1)
+% imagesc(max_corrs);
+% caxis([min(min(min_corrs)), max(max(max_corrs))])
+% colorbar
+% xlabel('Frequency')
+% ylabel('Subject')
+% saveas(gca, [save_dir, 'behavior_exp_corr_bl.png'], 'png')
+% save([R_dir, 'max_exp_b_corr_bl.mat'], 'max_corrs')
+% save([R_dir, 'min_exp_b_corr_bl.mat'], 'min_corrs')
+%
+% figure(2)
+% imagesc(min_corrs);
+% caxis([min(min(min_corrs)), max(max(max_corrs))])
+% colorbar
+% xlabel('Frequency')
+% ylabel('Subject')
+% saveas(gca, [save_dir, 'min_behavior_exp_corr_bl.png'], 'png')
+%
+% % plot realtionship between slope of learning and expression
+% for i = 1:numel(bands)
+%     figure(i); clf
+%     corrplot([betas(2:7)', improvement(2:7)', max_exp(2:end,i), min_exp(2:end,i), sum_exp(2:end, i), sd_exp(2:end,i)], 'Var', {'Beta', 'Im', 'Max', 'Min', 'mean', 'sd'}, 'type', 'spearman','testr', 'on')
+%     pause(0.001)
+% end
+%
+% figure(1); clf
+% corrplot([repmat(betas(2:7)',4,1), repmat(improvement(2:7)',4,1),  ...
+%     reshape(max_exp(2:end,:),[],1), reshape(min_exp(2:end,:),[],1), reshape(sum_exp(2:end, :), [],1), reshape(sd_exp(2:end, :), [],1)], 'Var',...
+%     {'Beta',  'Im', 'Max', 'Min', 'mean', 'sd'}, 'type', 'spearman','testr', 'on')
+%
---
+++ matlab/topo_node_exp_nb.m
@@ -0,0 +1,166 @@
+%% Topoplot Subgraph expression
+% make a topoplot of each nodes total participation in each subgraph
+% this moves things into the nodes space, and might washout effects of edge
+% space
+
+% this script if for unimodal matrices
+
+%% Global Variables
+
+addpath(genpath('/Users/stiso/Documents/MATLAB/npy-matlab-master/'))
+addpath('/Users/stiso/Documents/MATLAB/fieldtrip-20170830/')
+
+top_dir = '/Users/stiso/Documents/MATLAB/NetBCI/';
+data_dir = '/Users/stiso/Documents/Python/NetBCI/NMF/gc/';
+
+subjs = [1:20];
+nSubj = numel(subjs);
+nNode = 102;
+nEdge = (nNode^2-nNode)/2;
+freqs = [7,14;15,30;31,45;55,70];
+bands = [{'alpha'},{'beta'},{'low_gamma'},{'gamma'}];
+sensors = [{'grad'}];
+% threshold for edge visualization
+thr = 1;
+consensus = zeros(nNode,nNode, nSubj-1, numel(bands));
+
+%% Loop through data
+
+for i = subjs
+    subj = sprintf('%03d', i);
+    for j = 1:numel(sensors)
+        sens = sensors{j};
+        load([top_dir, 'idx_', sens, '.mat'])
+        for k = 1:numel(bands)
+            f = bands{k};
+            img_dir = [top_dir, 'NMF/', subj, '/', sens, '/images/'];
+            % make directories
+            if ~exist(img_dir, 'dir')
+                mkdir(img_dir)
+            end
+            save_dir = [top_dir, 'NMF/', subj, '/', sens, '/analysis/'];
+            % make directories
+            if ~exist(save_dir, 'dir')
+                mkdir(save_dir)
+            end
+            
+            % get subgraph data
+            coeff = readNPY([data_dir, subj,  '/', sens, '/nb_gc_', f, '_coeff.npy']);
+            subset = readNPY([data_dir, subj,  '/', sens, '/nb_gc_', f, '_subset.npy']);
+            err = readNPY([data_dir, subj,  '/', sens, '/nb_gc_', f, '_err.npy']);
+            % get mag index
+            labels = [];
+            try
+                load(['/Users/stiso/Resilio Sync/NETBCI.RAW/DataBase/1_Signals/0_RawData/2_MEG/Session1/1_test01/RawData_MEG_Subj', subj, '_Ses1_test01.mat'])
+                eval(['data = RawData_MEG_Subj', subj, '_Ses1_test01;']);
+                eval(['clear RawData_MEG_Subj', subj, '_Ses1_test01'])
+            catch
+                load(['/Users/stiso/Resilio Sync/NETBCI.RAW/DataBase/1_Signals/0_RawData/2_MEG/Session1/1_test01/RawData_MEG_Subj016_Ses1_test01.mat'])
+                eval(['data = RawData_MEG_Subj016_Ses1_test01;']);
+                eval(['clear RawData_MEG_Subj016_Ses1_test01'])
+            end
+            if strcmp(sens,'mag')
+                sens_idx = strcmp(data.meg_sensors.chantype,'megmag');
+                labels = data.label(sens_idx);
+            else
+                sens_idx = strcmp(data.meg_sensors.chantype,'megplanar');
+               grad_label = data.label(sens_idx);
+                cnt = 1;
+                for n = 1:2:sum(sens_idx)
+                    labels{cnt,1} = [grad_label{n}, '+', grad_label{n+1}(end-3:end)];
+                    cnt = cnt + 1;
+                end
+            end
+
+            % sizes
+            nTime = size(coeff,2);
+            nSG = size(subset,1);
+            nSens = numel(labels);
+            
+            for n = 1:nSG
+                % get expressin into matrix
+                curr_SG = subset(n,:);
+                node_exp = get_sg_matrix(nSens, curr_SG);
+                sg_deg = sum(node_exp)';
+               
+                % save edge file
+                % first threshold
+                v_graph = reshape(node_exp((node_exp ~= 0)), 1, []);
+                v_graph = sort(v_graph);
+                n_thrsh = floor(numel(v_graph)*thr);
+                v_thrsh = v_graph(end-n_thrsh+1);
+                node_exp(node_exp<v_thrsh) = 0;
+                dlmwrite([save_dir, f, '_graph_', num2str(n), '_nb.edge'], node_exp, '\t')
+                
+                % get into fieldtrip format for topoplot
+                cfg = [];
+                cfg.style = 'straight';
+                if strcmp('mag',sens)
+                    cfg.layout = [top_dir, 'layouts/neuromag306', sens, '.lay'];
+                    fid = fopen([top_dir, 'layouts/neuromag306', sens, '.txt'], 'r');
+                else
+                    cfg.layout = [top_dir, 'layouts/neuromag306cmb.lay'];
+                    fid = fopen([top_dir, 'layouts/neuromag306cmb.txt'], 'r');
+                end
+
+                
+                plot_data.powspctrm = sg_deg;
+                plot_data.label = labels;
+                plot_data.dimord = 'chan_freq';
+                plot_data.freq = 6;
+                plot_data.cfg = [];
+                figure(1); clf
+                ft_topoplotER(cfg,plot_data); colorbar
+                saveas(gca, [img_dir, f, '_', num2str(n), '_nb.png'], 'png')
+                
+                % sidebar - make node file
+                bv_label = textscan(fid, '%d %10f %10f %d %d %s', 'Delimiter', '\n');
+                fclose(fid);
+                write_bv_node([save_dir, f, '_graph_', num2str(n), '_nb.node'], bv_label{:,2},bv_label{:,3},double(bv_label{:,4}), 1, sg_deg);
+            end
+            
+        end
+    end
+end
+
+
+%% plot consensus - consistency (but without distance)
+% thrs = linspace(0.01, 1, 100);
+% nE_thr = zeros(numel(thrs),numel(bands));
+% consistent_edges = zeros(numel(thrs), numel(bands));
+% for k = 1:numel(thrs)
+%     thrsh_mats = consensus;
+%     for i = 1:nSubj
+%         for j = 1:numel(bands)
+%             thresh = thrs(k);
+%             curr = thrsh_mats(:,:,i,j);
+%             thresh = round(thresh*sum(sum(curr ~= 0)));
+%             tmp = curr(curr ~= 0);
+%             tmp = sort(tmp, 'descend');
+%             curr(curr < tmp(thresh)) = 0;
+%             nE_thr(k,j) = sum(sum(curr ~= 0));
+%             thrsh_mats(:,:,i,j) = curr;
+%         end
+%     end
+%     for m = 1:numel(bands)
+%         G = fcn_group_avg4(thrsh_mats(:,:,:,m),nSubj-1);
+%         consistent_edges(k,m) = ((sum(sum(G))/2)./nE_thr(k,m))*100;
+%         if k == numel(thrs)
+%             avg_consensus = mean(thrsh_mats(:,:,:,m),3);
+%             avg_consensus(~logical(G)) = 0;
+%             plot_data.powspctrm = sum(avg_consensus)';
+%             plot_data.label = labels;
+%             plot_data.dimord = 'chan_freq';
+%             plot_data.freq = 6;
+%             plot_data.cfg = [];
+%             figure(1); clf
+%             ft_topoplotER(cfg,plot_data); colorbar
+%             saveas(gca, ['/Users/stiso/Documents/MATLAB/NetBCI/GroupAvg/gc/images/', bands{m}, '_consensus_topo.png'], 'png')
+%         end
+%     end
+% end
+% % plot
+% plot(thrs,consistent_edges, 'linewidth', 2)
+% legend(bands)
+% saveas(gca, [img_dir, 'consensus_thresh.png'], 'png')
+
