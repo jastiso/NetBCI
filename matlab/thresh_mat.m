676ced7b43002bb74f478a766ab6f9527fe40ecd
jastiso <jeni.stiso@gmail.com>
2018-12-10 10:32:37 -0500

    made code for looking at temp exp of high1-3

---
+++ matlab/thresh_mat.m
@@ -0,0 +1,15 @@
+function [A_thresh] = thresh_mat(A,thr)
+%remove the thr% weakest connections in A
+% this assumes A is symmetric
+
+[edges] = sort(nonzeros(tril(A,-1)),'ascend');
+nEdge = numel(edges)*thr;
+rm_edges = edges(1:nEdge);
+A_thresh = tril(A,-1);
+for i = 1:numel(rm_edges)
+    A_thresh(A_thresh == rm_edges(i)) = 0;
+end
+A_thresh = A_thresh+A_thresh';
+
+end
+
