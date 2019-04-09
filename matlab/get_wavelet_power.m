2e5f5ecde46e3698f45d37e417ab95aa2767b1bf
jastiso <jeni.stiso@gmail.com>
2019-04-09 16:24:05 -0400

    wrote code to compare the number of triangles between WPLI and regularized, also made “brain states” for every trial

---
+++ matlab/get_wavelet_power.m
@@ -0,0 +1,33 @@
+function [ pow ] = get_wavelet_power( data, srate, st, en, freqs,t)
+%% Get power
+
+% getting power, using mtmfft
+
+%@author JStiso jeni.stiso@gmail.com
+
+% Change Log
+% April 8th - made script
+
+
+%% get cross spectral density
+% The product of the Fourier transform of one signal with the conjugate of the Fourier Transform of the signal gives the Cross-Spectral density (CSD).
+
+% this will be estimated with wavelet coherence, doesn't assume
+% stationarity, and gets you lots of time points
+% defining constants
+
+cfg = [];
+cfg.method     = 'mtmfft';
+cfg.tapsmofrq  = 0.5;
+cfg.output     = 'pow';
+cfg.foi        = freqs;
+cfg.toi        = st:1/srate:en;
+cfg.pad        = 'nextpow2';
+cfg.trials     = t;
+wave = ft_freqanalysis(cfg, data);
+
+% avg
+pow = squeeze(mean(wave.powspctrm,2));
+
+end
+
