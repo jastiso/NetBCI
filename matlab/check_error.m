f85b867b86c2dd7bcd500e0b46e39bae884ca954
jastiso <jeni.stiso@gmail.com>
2019-08-02 11:21:38 -0400

    deleted som eduplicates

--- matlab/check_error.m
+++
@@ -1,9 +0,0 @@
-function [] = check_error(err,tol)
-%compare error to tolerance, print warning
-
-if err > tol
-  warning ('Your error is too large. Check that you are reaching your target state. If not, try adding more entries to B, fewer to S, or using smaller matrices')
-end
-
-end
-
